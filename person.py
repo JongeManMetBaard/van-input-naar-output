bccf48363c7f31b12612f127d0fde2ce944bfd7f
ali <ali.gout.12@hotmail.com>
2021-09-13 15:20:41 +0200

    Gegevens van gebruiker

---
+++ person.py
@@ -0,0 +1,12 @@
+naam = input("Geef jou naam")
+adres = input("Geef jou adres")
+postcode = input("geef jou postcode")
+woonplaats = input("geef jou woonplaats")
+
+
+print ("----------------------------------------------------")
+print ("Naam      : " + naam)
+print ("Adres     : " + adres)
+print ("Postcode  : " + postcode)
+print ("Woonplaats: " + woonplaats)
+print ("----------------------------------------------------")
